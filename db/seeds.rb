09d5571934e5bab921193ca354c3b7972de1f72f
Yoshinori Tokuno <yoshi.jpn.10.11@gmail.com>
2024-11-10 17:34:28 +0900

    簡易的なseedファイルを定義

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,29 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+# test用アカウント
+User.create(email: "test@test.com", password: 'password', first_name: "太郎", last_name: "山田", confirmed_at: Time.now)
+
+5.times.each do |n|
+  User.create(email: "test#{n}@test.com", password: 'password', first_name: "#{n}郎", last_name: "山田", confirmed_at: Time.now)
+end
+
+Category.create(name: '開発')
+Category.create(name: 'マーケティング')
+Category.create(name: '営業')
+
+10.times.each do |n|
+  # MoneyのcurrencyについてのDEPRECATIONが表示されるが一旦money-railsの動向を見るため放置
+  Course.create(title: "Railsマスター_#{n}",
+                user: User.all[rand(User.count)],
+                category: Category.all[rand(Category.count)],
+                price: 10000,
+  )
+end
+
+Badge.create(name: '最高評価')
+Badge.create(name: 'ベストセラー')
+
+Course.all.each do |course|
+  next if rand(10) > 5
+
+  badge = Badge.all[rand(Badge.count)]
+  course.course_badges.create(badge: badge)
+end
