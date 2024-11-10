748335eebee4bea9a165282580acbfd90ebfcbfd
Yoshinori Tokuno <yoshi.jpn.10.11@gmail.com>
2024-11-10 17:00:56 +0900

    tailwind.config.jsにapp/components配下のファイル指定を追加

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   content: [
     './app/views/**/*.html.erb',
+    './app/components/**/*.rb',
     './app/helpers/**/*.rb',
     './app/assets/stylesheets/**/*.css',
     './app/javascript/**/*.js'
